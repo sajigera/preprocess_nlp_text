34ccf79942b2c6074a1d9a4ab0d872bf69a6987c
sajigera <saji.resni@gmail.com>
2024-01-01 02:33:10 +0530

    Python Packages for NLP Text Preprocessing

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', mode='r') as file:
+    long_description = file.read()
+    
+
+setuptools.setup(
+    name= 'preprocess_sajigera',
+    version = '0.0.1',
+    author= 'Saji Gera',
+    author_email= 'sajigera85@gmail.com',
+    description= 'This a preprocessing package.',
+    long_description=long_description,
+    long_description_content_type = 'text/markdown',
+    packages = setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT',
+        'Operating System :: OS Independent'],
+    python_requires = '>=3.5'        
+    
+)
